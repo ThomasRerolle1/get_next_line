04bcb0e75c11d769a5994beb364df09b600ff4ef
Tom <thomas.rerolle1997@gmail.com>
2022-01-11 00:58:24 +0100

    using read, create main

--- get_next_line.c
+++ get_next_line.c
@@ -1,8 +1,20 @@
-
 #include "get_next_line.h"
+#include <stdio.h>
 
 
 char	*get_next_line(int fd)
 {
 	static int	x = 0;
+	static char	buf[50];
+
+	read(fd, buf, sizeof(buf));
+	return (buf);
+}
+
+int	main()
+{
+	int	fd = open("test.txt", O_RDWR | O_CREAT, 0777);
+	printf("%s, %i", get_next_line(fd), fd);
+	write(2, "A", 1);
+	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,7 +1,11 @@
 #ifndef GET_NEXT_LINE_H
 #define GET_NEXT_LINE_H
-#include <unisd.h>
-#include <sdlib.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 
 #endif
